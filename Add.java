865415cacbb2b3e725432e969db9e9b9e051a737
unnatial2005 <meghana242005@gmail.com>
2025-04-09 08:53:45 +0530

    save to master

--- Add.java
+++ Add.java
@@ -1,8 +1,10 @@
- public class Add{
+public class Add{
     public static void main(String[] args) {
         int a=10;
         int b=20;
         int sum=a+b;
+        int sub=a-b;
         System.out.println("Sum of a and b is "+sum);
+        System.err.println("the sub of a and b: "+ sub);
     }
 }
